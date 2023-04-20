9bc5448267948fbb6cd707691344d094646f2ab8
meetpanchal1048 <meetpanchal1048@gmail.com>
2023-04-20 15:52:17 +0530

    Login

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ Array.from(document.getElementsByClassName("menu-item"))
             menu.dataset.activeIndex = index ;
         }
     })
+
+
+// ========================Login=========================
+
+const button = document.getElementById("myButton")
+const form = document.getElementById("myForm")
+
+button.addEventListener("click", function () {
+    form.style.display = "block";
+})
+
+
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username == "admin" && password == "admin"){
+        alert("Login Successfull")
+    }
+    else if (username == "hello" && password == "hello"){
+        window.open("test.html")
+    }
+}
